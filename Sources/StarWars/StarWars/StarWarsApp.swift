1575c2bb76cc7b95bbc6849e3c8bf56fe628a366
maticzav <matic.zavadlal@gmail.com>
2020-10-17 13:15:18 +0200

    :rocket: create workspace, add StarWars project

---
+++ Sources/StarWars/StarWars/StarWarsApp.swift
@@ -0,0 +1,17 @@
+//
+//  StarWarsApp.swift
+//  StarWars
+//
+//  Created by Matic Zavadlal on 17/10/2020.
+//
+
+import SwiftUI
+
+@main
+struct StarWarsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
