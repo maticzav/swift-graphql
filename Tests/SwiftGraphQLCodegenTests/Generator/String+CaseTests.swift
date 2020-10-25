a35f1ddbe6ead86c61c00779aa19bbe64d9e54ac
maticzav <matic.zavadlal@gmail.com>
2020-10-25 09:20:41 +0100

    :sparkles: casing test

--- Tests/SwiftGraphQLCodegenTests/Generator/String+CaseTests.swift
+++ Tests/SwiftGraphQLCodegenTests/Generator/String+CaseTests.swift
@@ -3,6 +3,7 @@ import XCTest
 
 final class StringExtensionsTest: XCTestCase {
     func testCamelCase() {
+        XCTAssertEqual("___a very peculiarNameIndeed__wouldNot.you.agree.AMAZING?____".camelCase, "aVeryPeculiarNameIndeedWouldNotYouAgreeAmazing")
         XCTAssertEqual("ENUM".camelCase, "enum")
         XCTAssertEqual("linkToURL".camelCase, "linkToUrl")
         XCTAssertEqual("grandfather_father.son grandson".camelCase, "grandfatherFatherSonGrandson")
