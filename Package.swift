d3f4d2e6b6d29f4b709b6f09889c56a54ac9af94
maticzav <matic.zavadlal@gmail.com>
2021-02-23 13:02:49 +0100

    fix: Add explicit platform specifictation
    Fixes #28.

--- Package.swift
+++ Package.swift
@@ -5,6 +5,12 @@ import PackageDescription
 
 let package = Package(
     name: "swift-graphql",
+    platforms: [
+        .iOS(.v13),
+        .macOS(.v10_15),
+        .tvOS(.v13),
+        .watchOS(.v6)
+    ],
     products: [
         /* SwiftGraphQL */
         .library(
