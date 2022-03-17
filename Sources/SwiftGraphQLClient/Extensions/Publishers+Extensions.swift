2c097577697beda21caacbcbfd2ec4c8b7655209
Matic Zavadlal <matic.zavadlal@gmail.com>
2022-03-17 15:29:36 -0400

    fix the stateful publisher

--- Sources/SwiftGraphQLClient/Extensions/Publishers+Extensions.swift
+++ Sources/SwiftGraphQLClient/Extensions/Publishers+Extensions.swift
@@ -11,6 +11,15 @@ extension Publisher {
             onEnd: {}
         )
     }
+    
+    /// An operator that triggers the handler when the publisher sends the subscription down to the subscriber.
+    func onEnd(_ handler: @escaping () -> Void) -> Publishers.StatefulPublisher<Self> {
+        Publishers.StatefulPublisher<Self>(
+            upstream: self,
+            onStart: {},
+            onEnd: handler
+        )
+    }
 }
 
 extension Publishers {
@@ -32,6 +41,7 @@ extension Publishers {
         /// A function called when the subscriber receives a completion event.
         private var onEnd: () -> Void
         
+        /// Publisher emitting the values.
         private var upstream: Upstream
         
         // MARK: - Initializer
@@ -49,47 +59,91 @@ extension Publishers {
         // MARK: - Publisher
         
         func receive<S>(subscriber: S) where S : Subscriber, Failure == S.Failure, Output == S.Input {
-            self.upstream.receive(subscriber: subscriber)
-            onStart()
+            // Wraps the actual subscriber inside a custom subscribers and subscribes to the upstream publisher with it.
+            let sub = Subscriptions.StatefulSubscription(
+                subscriber: subscriber,
+                onStart: self.onStart,
+                onEnd: self.onEnd
+            )
+            subscriber.receive(subscription: sub)
+            self.upstream.receive(subscriber: sub)
         }
     }
 }
 
-//extension Subscriptions {
-//    class StatefulSubscription<
-//        Upstream: Subscription,
-//        Downstream: Subscriber
-//    >: Subscription {
-//        
-//        var subscriber: Downstream?
-//        var upstream: Subscription
-//        
-//        init(subscriber: Downstream, upstream: Subscription) {
-//            self.subscriber = subscriber
-//            self.upstream = upstream
-//        }
-//        
-//        func receive(subscription: Subscription) {
-//            self.subscription = subscription
-//        }
-//        
-//        func receive(_ input: T) -> Subscribers.Demand {
-//            self.subscriber?.receive(input) ?? .none
-//        }
-//        
-//        func receive(completion: Subscribers.Completion<E>) {
-////            onEnd()
-//            self.subscriber?.receive(completion: completion)
-//        }
-//        
-//        func request(_ demand: Subscribers.Demand) {
-//            self.upstream.request(demand)
-//        }
-//        
-//        func cancel() {
-//            self.upstream.cancel()
-//        }
-//        
-//        
-//    }
-//}
+extension Subscriptions {
+    
+    /// A subscription that republishes events from the upstream and watches
+    /// for the construction and teardown of the pipeline.
+    class StatefulSubscription<Downstream: Subscriber>: Subscription, Subscriber {
+        
+        typealias Input = Downstream.Input
+        typealias Failure = Downstream.Failure
+        
+
+        /// Subscription that yields values we use to stream down.
+        private var subscription: Subscription?
+        
+        /// The subscriber we are forwarding values to.
+        private var subscriber: Downstream
+        
+        /// Cached demand.
+        private var demand: Subscribers.Demand = .none
+        
+        /// A function that's triggered when the publisher establishes a connection with the subscriber.
+        private var onStart: () -> Void
+        
+        /// A function called when the subscriber receives a completion event.
+        private var onEnd: () -> Void
+        
+
+        init(
+            subscriber: Downstream,
+            onStart: @escaping () -> Void,
+            onEnd: @escaping () -> Void
+        ) {
+            self.subscriber = subscriber
+            self.onStart = onStart
+            self.onEnd = onEnd
+        }
+        
+        // MARK: - Subscriber
+
+        func receive(subscription: Subscription) {
+            self.subscription = subscription
+            
+            if self.demand > 0 {
+                self.subscription?.request(self.demand)
+                self.demand = .none
+            }
+            
+            onStart()
+        }
+
+        func receive(_ input: Downstream.Input) -> Subscribers.Demand {
+            self.subscriber.receive(input)
+        }
+
+        func receive(completion: Subscribers.Completion<Downstream.Failure>) {
+            onEnd()
+            self.subscriber.receive(completion: completion)
+        }
+
+        // MARK: - Subscription
+        
+        func request(_ demand: Subscribers.Demand) {
+            guard let subscription = subscription else {
+                self.demand += demand
+                return
+            }
+
+            subscription.request(demand)
+        }
+
+        func cancel() {
+            self.subscription?.cancel()
+            self.subscription = nil
+        }
+
+    }
+}
--- Tests/SwiftGraphQLClientTests/Extensions/Publishers+ExtensionsTests.swift
+++ Tests/SwiftGraphQLClientTests/Extensions/Publishers+ExtensionsTests.swift
@@ -1,8 +1,9 @@
+import Combine
 @testable import SwiftGraphQLClient
-
 import XCTest
 
 final class PublishersExtensionsTests: XCTestCase {
+    
     func testOnStartTriggeredCorrectly() throws {
         var i = 0
         var received = [Int]()
@@ -23,5 +24,32 @@ final class PublishersExtensionsTests: XCTestCase {
         
         XCTAssertEqual(i, 1)
         XCTAssertEqual(received, [1, 2, 3])
+    }
+    
+    var cancellables = Set<AnyCancellable>()
+    
+    func testOnEndTriggeredCorrectly() throws {
+        var i = 0
+        var received = [Int]()
+        
+        let subject = PassthroughSubject<Int, Never>()
+        let _ = subject
+            .eraseToAnyPublisher()
+            .onEnd {
+                i += 1
+            }
+            .sink { val in
+                received.append(val)
+            }
+            .store(in: &self.cancellables)
+        
+        subject.send(42)
+        
+        XCTAssertEqual(i, 0)
+        
+        subject.send(completion: .finished)
+        
+        XCTAssertEqual(i, 1)
+        XCTAssertEqual(received, [42])
     }
 }
