1d635f8adbac5bc554e44a45558e5da5258b585b
Robson Tenório <rrtenorio@gmail.com>
2023-06-16 19:57:42 -0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,11 +1,11 @@
-// swift-tools-version:5.6
+// swift-tools-version:5.7
 
 import PackageDescription
 
 let package = Package(
     name: "swift-graphql",
     platforms: [
-        .iOS(.v15_1),
+        .iOS(.v16),
         .macOS(.v10_15),
         .tvOS(.v13),
         .watchOS(.v6)
