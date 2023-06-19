0388098ff0e40442f4b21e8b6fc1251dde0074bb
Robson Tenório <rrtenorio@gmail.com>
2023-06-19 05:54:53 -0300

    fix: Update iOS version requirements
    NOTE: This is not a breaking change, because the code hasn't fundamentally changed since the introduction of version 5.0.0, this is just a fix that makes version 5.0.0 work correctly.
    
    Co-authored-by: Matic Zavadlal <matic.zavadlal@gmail.com>

--- Package.swift
+++ Package.swift
@@ -1,11 +1,11 @@
-// swift-tools-version:5.6
+// swift-tools-version:5.7
 
 import PackageDescription
 
 let package = Package(
     name: "swift-graphql",
     platforms: [
-        .iOS(.v13),
+        .iOS(.v15),
         .macOS(.v10_15),
         .tvOS(.v13),
         .watchOS(.v6)
