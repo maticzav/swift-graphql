af08735c1ac2bc1cc5a2c5cd716d3c57cee75751
maticzav <maticzav@users.noreply.github.com>
2023-07-04 07:13:19 +0000

    Bump Podspec and Brew formula

--- Formula/SwiftGraphQL.rb
+++ Formula/SwiftGraphQL.rb
@@ -3,8 +3,8 @@ class Swiftgraphql < Formula
   homepage "https://swift-graphql.org"
   license "MIT"
   
-  url "https://github.com/maticzav/swift-graphql/archive/5.0.3.tar.gz"
-  sha256 "9d896ecb1cded8da2007994ece4457a6b833f0e48b23c23c504ce7227949623a"
+  url "https://github.com/maticzav/swift-graphql/archive/5.0.4.tar.gz"
+  sha256 "ee77772ef29c64ee54b4b372f77d064b9bad751eb2383520f84c456f19acbfee"
   
   head "https://github.com/maticzav/swift-graphql.git"
 
