6efc8cae779bbf3b625c2dacb00965d98aa5fd72
Shaps Benkau <shapsuk@me.com>
2023-10-04 20:30:38 +0100

    Now compiles with updated Starscream APIs

--- Sources/GraphQLWebSocket/Client.swift
+++ Sources/GraphQLWebSocket/Client.swift
@@ -74,7 +74,7 @@ public class GraphQLWebSocket: WebSocketDelegate {
         case connecting
         
         /// WebSocket has opened.
-        case opened(socket: WebSocket)
+        case opened(socket: WebSocketClient)
         
         /// Open WebSocket connection has been acknowledged
         case acknowledged(payload: [String: AnyCodable]?)
@@ -143,7 +143,7 @@ public class GraphQLWebSocket: WebSocketDelegate {
 
     // MARK: - Internals
     
-    public func didReceive(event: WebSocketEvent, client: WebSocket) {
+    public func didReceive(event: WebSocketEvent, client: WebSocketClient) {
         self.config.logger.debug("Received a new message from the server!")
         
         switch event {
