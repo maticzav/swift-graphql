cdd78423a829aa766eaccfe66c6615f709ba22dd
pokryfka <dev@pokryfka.net>
2023-10-24 17:50:11 +0800

    chore: sync platform versions

--- Package.swift
+++ Package.swift
@@ -6,9 +6,9 @@ let package = Package(
     name: "swift-graphql",
     platforms: [
         .iOS(.v15),
-        .macOS(.v10_15),
-        .tvOS(.v13),
-        .watchOS(.v6)
+        .macOS(.v12),
+        .tvOS(.v15),
+        .watchOS(.v8)
     ],
     products: [
         // SwiftGraphQL
