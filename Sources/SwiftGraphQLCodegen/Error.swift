5b62e7e2a4a30aa6706431a4d7189fd76dfdec02
Jairon Terrero <jairon@passivelogic.com>
2024-02-27 13:04:02 -0700

    chore: Move code gen to separate package
    This is primarily beneficial because it removes dev related dependencies from the Swift package.
    This will make it play nicer as an import because it won't force consumers to clone and build all the non-product dependencies.

--- Sources/SwiftGraphQLCodegen/Error.swift
+++
@@ -1,8 +0,0 @@
-import Foundation
-
-/// Errors that might occur during the code-generation phase.
-public enum CodegenError: Error {
-    
-    /// There was a problem with the formatting of the library.
-    case formatting(Error)
-}
